cec4f0d5d47227c5842d1dbabc54559c35a4e6b3
Noah Bumgardner <noah@playgamecafe.com>
2024-09-01 07:59:43 -0500

    Add Notes to Chromatic Scale

--- v2-godot/scales/scale_data.gd
+++ v2-godot/scales/scale_data.gd
@@ -2,4 +2,5 @@ class_name ScaleData extends Resource
 
 @export var full_name = "full_display_name"
 @export var label_name = "Any Scale"
+@export var solfege_ascending_notes: Array[SolfegeNoteData] = []
 @export var solfege_ascending_string = "Do di Re ri Mi Fa fi So si La li Ti"
