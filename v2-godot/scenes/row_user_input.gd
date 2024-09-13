359a71d177e06245a857a6d964df73d9d3dc860e
Noah Bumgardner <noah@playgamecafe.com>
2024-09-12 20:21:34 -0500

    Output Signal of Text on Translate Click

---
+++ v2-godot/scenes/row_user_input.gd
@@ -0,0 +1,16 @@
+extends HBoxContainer
+
+signal request_to_translate
+
+@onready var input_control_to_translate: LineEdit = (
+	$LineEdit
+)
+
+func _on_button_pressed():
+	# Get string to be translated.
+	var raw_text: String = input_control_to_translate.text
+	if raw_text == '': # Guardian.
+		print_debug('NO INPUT FOUND. Please enter text for translation.')
+		return
+
+	print_debug('Requesting to translate: ', raw_text)
