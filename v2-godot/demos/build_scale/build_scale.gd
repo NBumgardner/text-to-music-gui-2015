7ba994626c66f1c7842b91dbbee0b5fa69ec3523
Noah Bumgardner <noah@playgamecafe.com>
2024-09-28 08:27:13 -0500

    Play Different 2nd Note in Build Scale Demo

--- v2-godot/demos/build_scale/build_scale.gd
+++ v2-godot/demos/build_scale/build_scale.gd
@@ -2,6 +2,23 @@
 extends Control
 
 const note_c_natural_4 = preload("res://audio/c-nat-4.wav")
+const note_d_natural_4 = preload("res://audio/d-nat-4.wav")
+
+
+func _ready():
+	var scale_to_build = _build_stream_sequential()
+	var notes_to_append_list = [note_c_natural_4, note_d_natural_4]
+
+	for note_index in notes_to_append_list.size():
+		scale_to_build.add_stream(note_index, notes_to_append_list[note_index])
+
+	$AudioStreamPlayer.set_stream(scale_to_build)
+
+
+func _build_stream_sequential() -> AudioStreamRandomizer:
+	var scale_to_build = AudioStreamRandomizer.new()
+	scale_to_build.playback_mode = scale_to_build.PLAYBACK_SEQUENTIAL
+	return scale_to_build
 
 
 func _on_play_button_pressed():
@@ -19,7 +36,7 @@ func _on_prepend_note_button_pressed():
 	else:
 		print_debug('Setting audio.')
 
-	$AudioStreamPlayer.stream = note_c_natural_4
+	$AudioStreamPlayer.stream.add_stream(-1, note_c_natural_4)
 
 
 func _on_reset_button_pressed():
@@ -28,4 +45,4 @@ func _on_reset_button_pressed():
 		return
 
 	print_debug('Resetting audio.')
-	$AudioStreamPlayer.stream = null
+	$AudioStreamPlayer.set_stream(_build_stream_sequential())
