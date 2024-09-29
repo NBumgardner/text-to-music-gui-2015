dbfda4daa9e31dae51e2f35e33c37a0d6d623c90
Noah Bumgardner <noah@playgamecafe.com>
2024-09-29 12:13:27 -0500

    Encapsulate Populating Sequential Stream

--- v2-godot/demos/build_scale/build_scale.gd
+++ v2-godot/demos/build_scale/build_scale.gd
@@ -10,28 +10,14 @@ const note_c_natural_4 = preload("res://audio/c-nat-4.wav")
 	"res://data/scales/pentatonic.tres"
 )
 
+var solfege_notes_of_stream: Array[SolfegeNoteData] = []
+
 
 func _ready():
-	var scale_to_build = _build_stream_sequential()
-	var notes_to_append_list = []
-	var chromatic_index_maximum = chromatic_index_to_streams.pitch_list.size()
-
-	if chromatic_index_maximum == 0:
-		print_debug(
-			'Error: chromatic_index_to_streams of ',
-			chromatic_index_to_streams.description,
-			' is missing audio files.'
-		)
-
-	notes_to_append_list = musical_scale.solfege_ascending_notes.map(
-		func(solfegeNote: SolfegeNoteData):
-			return chromatic_index_to_streams.pitch_list[
-				solfegeNote.chromatic_index % chromatic_index_maximum
-			]
+	solfege_notes_of_stream = musical_scale.solfege_ascending_notes
+	var scale_to_build = _build_stream_sequential_from_solfege_note_data(
+		solfege_notes_of_stream
 	)
-
-	for note_index in notes_to_append_list.size():
-		scale_to_build.add_stream(note_index, notes_to_append_list[note_index])
 
 	$AudioStreamPlayer.set_stream(scale_to_build)
 
@@ -43,6 +29,37 @@ func _ready():
 func _build_stream_sequential() -> AudioStreamRandomizer:
 	var scale_to_build = AudioStreamRandomizer.new()
 	scale_to_build.playback_mode = scale_to_build.PLAYBACK_SEQUENTIAL
+	return scale_to_build
+
+
+func _build_stream_sequential_from_solfege_note_data(
+	solfege_note_data: Array[SolfegeNoteData]
+) -> AudioStreamRandomizer:
+	var scale_to_build = _build_stream_sequential()
+	# Type of `Array[AudioStream]`.
+	var audio_stream_notes_to_append_list = []
+	var chromatic_index_maximum = chromatic_index_to_streams.pitch_list.size()
+
+	if chromatic_index_maximum == 0:
+		print_debug(
+			'Error: chromatic_index_to_streams of ',
+			chromatic_index_to_streams.description,
+			' is missing audio files.'
+		)
+
+	audio_stream_notes_to_append_list = solfege_note_data.map(
+		func(solfegeNote: SolfegeNoteData) -> AudioStream:
+			return chromatic_index_to_streams.pitch_list[
+				solfegeNote.chromatic_index % chromatic_index_maximum
+			]
+	)
+
+	for note_index in audio_stream_notes_to_append_list.size():
+		scale_to_build.add_stream(
+			note_index,
+			audio_stream_notes_to_append_list[note_index]
+		)
+
 	return scale_to_build
 
 
