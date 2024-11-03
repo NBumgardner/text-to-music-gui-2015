ff6908dbc367323b99d3935f47f8d9ef4daa3096
Noah Bumgardner <noah@playgamecafe.com>
2024-11-02 23:05:33 -0500

    Play All 3 Notes Sequentially Ascending

--- v2-godot/demos/play_any_note/play_any_note.gd
+++ v2-godot/demos/play_any_note/play_any_note.gd
@@ -1,8 +1,22 @@
 extends Control
 
 
+const _all_note_indexes_ascending_list: Array[int] = [0, 2, 4]
+const _note_length_seconds: float = 1
 const _volume_mute_decibel = -60
 const _volume_normal_decibel = 0
+
+
+var queue_of_note_indexes_to_play: Array = []
+var time_since_last_note_started: float = 0
+
+
+func _process(delta: float) -> void:
+	time_since_last_note_started += delta
+
+	if (time_since_last_note_started >= _note_length_seconds
+		and not queue_of_note_indexes_to_play.is_empty()):
+		_play_note_at_index(queue_of_note_indexes_to_play.pop_front())
 
 
 func _mute_streams():
@@ -23,6 +37,11 @@ func _on_button_d_4_pressed():
 func _on_button_e_4_pressed():
 	_play_note_at_index(4)
 
+
+func _on_button_play_all_sequentially_pressed():
+	queue_of_note_indexes_to_play.append_array(_all_note_indexes_ascending_list)
+
+
 #endregion Button pressed
 
 
@@ -33,4 +52,4 @@ func _play_note_at_index(note_index: int) -> void:
 		_volume_normal_decibel % $AudioStreamPlayer.stream.get_stream_count()
 	)
 	$AudioStreamPlayer.play()
-	
+	time_since_last_note_started = 0
