efd7e9757867f688371adaf9e475cd9925041b24
Noah Bumgardner <noah@playgamecafe.com>
2024-12-30 10:14:48 -0600

    Control Scale Rows via Exported Scale List

--- v2-godot/demos/mimic_python_guid/mimic_python_guid.gd
+++ v2-godot/demos/mimic_python_guid/mimic_python_guid.gd
@@ -5,6 +5,7 @@ extends Control
 @export var _mock_scale_chromatic: ScaleData = preload(
 	"res://data/scales/chromatic.tres"
 )
+@export var _scale_list: Array[ScaleData] = []
 
 
 @onready var input_text_line_edit: LineEdit = $GridContainer/LineEdit
@@ -25,11 +26,25 @@ const _translated_notes_separator = ' '
 
 
 var _converter_methods = _convert.new()
-var _mock_scale_used_chromatic = (
-	_mock_scale_chromatic.solfege_ascending_string.split(
-		' '
+var _scale_in_solfege_list = []
+
+
+func _get_scale_label_list():
+	var grid_cells_list = $GridContainer.get_children()
+	var grid_cells_labels_list = grid_cells_list.filter(
+		func (child): return child is Label
 	)
-)
+	return grid_cells_labels_list.slice(1)
+
+
+func _ready():
+	var scale_label_list = _get_scale_label_list()
+	var scale_label_relative_row_index = 0
+	for scale_label in scale_label_list:
+		scale_label.text = _scale_list[
+			scale_label_relative_row_index
+		].label_name
+		scale_label_relative_row_index += 1
 
 
 func _on_translate_button_pressed():
@@ -61,7 +76,11 @@ func _on_translate_button_pressed():
 				scale_line_edit.text,
 				' will be replaced.'
 			)
-		var scale_used = _mock_scale_used_chromatic
+		var scale_used = _scale_list[
+			scale_line_edit_relative_row_index
+		].solfege_ascending_string.split(
+			' '
+		)
 		var scale_line_edit_updated_text = _converter_methods.iListsToSolfege(
 			words_as_number_format_list,
 			scale_used
