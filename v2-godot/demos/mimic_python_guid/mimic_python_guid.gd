f32872812eac52f95305017b9d730dfc25635d0d
Noah Bumgardner <noah@playgamecafe.com>
2024-12-30 10:26:01 -0600

    Loop to Find Line Edits Instead of Hardcoding

--- v2-godot/demos/mimic_python_guid/mimic_python_guid.gd
+++ v2-godot/demos/mimic_python_guid/mimic_python_guid.gd
@@ -9,12 +9,6 @@ extends Control
 
 
 @onready var input_text_line_edit: LineEdit = $GridContainer/LineEdit
-@onready var scale_line_edit_list = [
-	$GridContainer/LineEdit2,
-	$GridContainer/LineEdit3,
-	$GridContainer/LineEdit4,
-	$GridContainer/LineEdit5
-]
 
 
 const _convert = preload("../../musical_translations/convert.gd")
@@ -27,24 +21,35 @@ const _translated_notes_separator = ' '
 
 var _converter_methods = _convert.new()
 var _scale_in_solfege_list = []
+var _scale_line_edit_list = []
+
+
+func _get_scale_line_edit_list():
+	var grid_cells_list = $GridContainer.get_children()
+	var grid_cells_line_edit_list = grid_cells_list.filter(
+		func (child): return child is LineEdit
+	)
+	return grid_cells_line_edit_list.slice(1)
 
 
 func _get_scale_label_list():
 	var grid_cells_list = $GridContainer.get_children()
-	var grid_cells_labels_list = grid_cells_list.filter(
+	var grid_cells_label_list = grid_cells_list.filter(
 		func (child): return child is Label
 	)
-	return grid_cells_labels_list.slice(1)
+	return grid_cells_label_list.slice(1)
 
 
 func _ready():
+	_scale_line_edit_list = _get_scale_line_edit_list()
+
 	var scale_label_list = _get_scale_label_list()
-	var scale_label_relative_row_index = 0
-	for scale_label in scale_label_list:
-		scale_label.text = _scale_list[
-			scale_label_relative_row_index
+
+	for scale_relative_row_index in range(scale_label_list.size()):
+		scale_label_list[scale_relative_row_index].text = _scale_list[
+			scale_relative_row_index
 		].label_name
-		scale_label_relative_row_index += 1
+		scale_relative_row_index += 1
 
 
 func _on_translate_button_pressed():
@@ -68,8 +73,8 @@ func _on_translate_button_pressed():
 		)
 	print_debug('Input Text in number format: ', words_as_number_format_list)
 
-	var scale_line_edit_relative_row_index = 0
-	for scale_line_edit in scale_line_edit_list:
+	var scale_relative_row_index = 0
+	for scale_line_edit in _scale_line_edit_list:
 		if scale_line_edit.text != '':
 			print(
 				_debug_message_indentation,
@@ -77,7 +82,7 @@ func _on_translate_button_pressed():
 				' will be replaced.'
 			)
 		var scale_used = _scale_list[
-			scale_line_edit_relative_row_index
+			scale_relative_row_index
 		].solfege_ascending_string.split(
 			' '
 		)
@@ -88,7 +93,7 @@ func _on_translate_button_pressed():
 		print_debug(
 			_debug_message_indentation,
 			'Row index ',
-			scale_line_edit_relative_row_index,
+			scale_relative_row_index,
 			' translated numbers ',
 			words_as_number_format_list,
 			' into solfege \'',
@@ -98,10 +103,10 @@ func _on_translate_button_pressed():
 			' solfege symbols.'
 		)
 		scale_line_edit.text = scale_line_edit_updated_text
-		scale_line_edit_relative_row_index += 1
+		scale_relative_row_index += 1
 	print_debug('Translation complete.')
 
 
 func _set_scales_line_edits(translated_text):
-	for scale_line_edit in scale_line_edit_list:
+	for scale_line_edit in _scale_line_edit_list:
 		scale_line_edit.text = translated_text
