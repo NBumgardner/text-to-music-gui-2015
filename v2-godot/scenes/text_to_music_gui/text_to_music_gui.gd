10dc932ed9395f63c1ad017dea7a5f693f8f5738
Noah Bumgardner <noah@playgamecafe.com>
2026-02-18 16:46:25 -0600

    Stop Playing Rows When Play is Clicked

--- v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
+++ v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
@@ -8,10 +8,16 @@ var converter_methods = Convert.new()
 	$MarginContainer/VBoxContainer/RowUserInput/LineEdit
 )
 
-# Get output rows excluding the first row of user input.
+# Get output rows excluding the first row and last rows of user input.
 @onready var play_var_list: Array[Node] = (
 	$MarginContainer/VBoxContainer.get_children()
 		.filter(func (child): return child is ScaledSolfegePlayerFirstNote)
+)
+
+# Get output rows excluding the first 5 row of user input.
+@onready var scaled_solfege_player_list: Array[Node] = (
+	$MarginContainer/VBoxContainer.get_children()
+		.filter(func (child): return child is ScaledSolfegePlayer)
 )
 
 var _scale_in_solfege: Array[ScaleData] = []
@@ -22,3 +28,8 @@ func _ready():
 		func(playVar: Node) -> ScaleData: return playVar.musical_scale
 	))
 	_scale_in_solfege.assign(incoming_scales_in_solfege)
+
+
+func _on_scaled_solfege_player_play_button_pressed():
+	for scaled_solfege_player in scaled_solfege_player_list:
+		scaled_solfege_player.stop_notes()
