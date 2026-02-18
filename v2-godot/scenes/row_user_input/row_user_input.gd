d4102297f73e5275cff9728ef60277267b582889
Noah Bumgardner <noah@playgamecafe.com>
2026-02-18 14:52:57 -0600

    Document Only Godot Signal for Clarity

--- v2-godot/scenes/row_user_input/row_user_input.gd
+++ v2-godot/scenes/row_user_input/row_user_input.gd
@@ -1,10 +1,16 @@
 extends HBoxContainer
 
+## Signal called with the contents of the [LineEdit] when the [i]Translate[/i]
+##  button is pressed.
+## [br][br]
+## Also called when pressing [i]Enter[/i] on a keyboard while focused on the
+##  [LineEdit].
 signal request_to_translate(raw_text)
 
 @onready var input_control_to_translate: LineEdit = (
 	$LineEdit
 )
+
 
 func _on_button_pressed():
 	# Get string to be translated.
@@ -17,5 +23,5 @@ func _on_button_pressed():
 	request_to_translate.emit(raw_text)
 
 
-func _on_line_edit_text_submitted(new_text):
+func _on_line_edit_text_submitted(_new_text):
 	$Button.pressed.emit()
