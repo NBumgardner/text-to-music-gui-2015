8ccc508e8516050c10f1bcc660e37f02546e6d24
Noah Bumgardner <noah@playgamecafe.com>
2026-02-19 14:57:22 -0600

    Hide Description Until Select, Bad Color

--- v2-godot/scenes/settings_menu/settings_menu.gd
+++ v2-godot/scenes/settings_menu/settings_menu.gd
@@ -5,5 +5,13 @@ extends MarginContainer
 signal turbo_mode_toggle_requested(toggled_on: bool)
 
 
+@onready var turbo_mode_description = $VBoxContainer/TurboModeDescription
+
+
+func _ready():
+	turbo_mode_description.set_modulate(Color(255, 255, 255, 0))
+
+
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	turbo_mode_toggle_requested.emit(toggled_on)
+	turbo_mode_description.set_modulate(Color(255, 255, 255, 255))
