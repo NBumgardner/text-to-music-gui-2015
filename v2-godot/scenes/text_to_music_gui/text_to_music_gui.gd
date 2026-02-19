ec64d1dc1c2b0be2e9d794aed883ad1cb42c4912
Noah Bumgardner <noah@playgamecafe.com>
2026-02-19 08:48:43 -0600

    Shrink PR: Cut Unused Scenes and Demos

--- v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
+++ v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
@@ -1,33 +1,11 @@
 extends Control
 
-const Convert = preload("../../musical_translations/convert.gd")
 
-var converter_methods = Convert.new()
-
-@onready var input_control_to_translate: LineEdit = (
-	$MarginContainer/VBoxContainer/RowUserInput/LineEdit
-)
-
-# Get output rows excluding the first row and last rows of user input.
-@onready var play_var_list: Array[Node] = (
-	$MarginContainer/VBoxContainer.get_children()
-		.filter(func (child): return child is ScaledSolfegePlayerFirstNote)
-)
-
-# Get output rows excluding the first 5 row of user input.
+# Get output rows excluding the first row of user input.
 @onready var scaled_solfege_player_list: Array[Node] = (
 	$MarginContainer/VBoxContainer.get_children()
 		.filter(func (child): return child is ScaledSolfegePlayer)
 )
-
-var _scale_in_solfege: Array[ScaleData] = []
-
-
-func _ready():
-	var incoming_scales_in_solfege: Array = (play_var_list.map(
-		func(playVar: Node) -> ScaleData: return playVar.musical_scale
-	))
-	_scale_in_solfege.assign(incoming_scales_in_solfege)
 
 
 func _on_scaled_solfege_player_play_button_pressed():
