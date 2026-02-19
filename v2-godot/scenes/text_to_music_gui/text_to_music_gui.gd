5a0ed079a945d9d41afd3cced1d261850c469d3d
Noah Bumgardner <noah@playgamecafe.com>
2026-02-19 14:41:36 -0600

    Add Settings Menu to Render on Button Click

--- v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
+++ v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
@@ -8,6 +8,17 @@ extends Control
 )
 
 
+@onready var settings_menu = $SettingsMenu
+
+
+func _ready():
+	settings_menu.visible = false
+
+
+func _on_button_settings_pressed():
+	settings_menu.visible = !settings_menu.visible
+
+
 func _on_scaled_solfege_player_play_button_pressed():
 	for scaled_solfege_player in scaled_solfege_player_list:
 		scaled_solfege_player.stop_notes()
