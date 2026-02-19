ccdb2a03d5cbf3b04f785082a6b56c3734bf985d
Noah Bumgardner <noah@playgamecafe.com>
2026-02-19 15:06:30 -0600

    Explain Calculation of Note Length in Code

--- v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
+++ v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
@@ -25,13 +25,15 @@ func _on_scaled_solfege_player_play_button_pressed():
 
 
 func _on_settings_menu_turbo_mode_toggle_requested(toggled_on):
-	if toggled_on:
-		for scaled_solfege_player in scaled_solfege_player_list:
-			scaled_solfege_player.note_length_seconds = (
-				scaled_solfege_player.note_length_seconds / 2
-			)
-	else:
-		for scaled_solfege_player in scaled_solfege_player_list:
-			scaled_solfege_player.note_length_seconds = (
-				scaled_solfege_player.note_length_seconds * 2
-			)
+	# When toggling on, play notes twice as fast by lasting half as long.
+	var note_length_multiplier = 0.5
+	
+	if not toggled_on:
+		# When toggling off, invert the multiplication to restore original note
+		#  lengths.
+		note_length_multiplier = 1 / note_length_multiplier
+
+	for scaled_solfege_player in scaled_solfege_player_list:
+		scaled_solfege_player.note_length_seconds = (
+			scaled_solfege_player.note_length_seconds * note_length_multiplier
+		)
