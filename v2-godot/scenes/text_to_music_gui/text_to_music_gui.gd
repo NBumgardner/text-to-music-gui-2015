7a2a659efe0dc3aeea095c73717574631a3821ed
Noah Bumgardner <noah@playgamecafe.com>
2026-02-19 19:08:01 -0600

    Fix Playback Multiplication Logic

--- v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
+++ v2-godot/scenes/text_to_music_gui/text_to_music_gui.gd
@@ -35,16 +35,22 @@ func _on_button_mouse_entered():
 	sfx_mouse_hover.play()
 
 
+## When toggling on, play notes twice as fast by lasting half as long.
+## When toggling off, invert the multiplication to restore original note
+##  lengths.
 func _on_settings_menu_playback_speed_toggle_requested(toggled_on, multiplier):
-	# When toggling on, play notes twice as fast by lasting half as long.
 	var note_length_multiplier = multiplier
 
-	if not toggled_on:
-		# When toggling off, invert the multiplication to restore original note
-		#  lengths.
-		note_length_multiplier = 1 / note_length_multiplier
+	if toggled_on:
+		note_length_multiplier = 1.0 / note_length_multiplier
 
+	_set_note_lengths_by_multiplier(note_length_multiplier)
+
+
+## Adjust each [ScaledSolfegePlayer]'s audio playback note length by multiplying
+##  by [multiplier].
+func _set_note_lengths_by_multiplier(multiplier):
 	for scaled_solfege_player in scaled_solfege_player_list:
 		scaled_solfege_player.note_length_seconds = (
-			scaled_solfege_player.note_length_seconds * note_length_multiplier
+			scaled_solfege_player.note_length_seconds * multiplier
 		)
