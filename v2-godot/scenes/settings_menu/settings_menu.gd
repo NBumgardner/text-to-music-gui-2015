f6df9820f52bf607191971be149ece3b1a1c49e8
Noah Bumgardner <noah@playgamecafe.com>
2026-07-08 11:56:29 -0500

    Add an Unimplemented Option to Hide Text

--- v2-godot/scenes/settings_menu/settings_menu.gd
+++ v2-godot/scenes/settings_menu/settings_menu.gd
@@ -5,6 +5,10 @@ extends MarginContainer
 signal playback_speed_toggle_requested(toggled_on: bool, multiplier: float)
 
 
+## Emitted when the [i]Hide button labels[/i] checkbox is toggled on or off.
+signal hide_button_labels_toggle_requested(toggled_on: bool)
+
+
 ## Emitted when the [i]Close[/i] [Button] is clicked.
 signal close_requested()
 
@@ -12,6 +16,9 @@ signal close_requested()
 @export var note_length_multiplier = 2
 
 
+@onready var checkbox_hide_button_labels = (
+	$MarginContainer/VBoxContainer/CheckBoxHideButtonLabels
+)
 @onready var checkbox_playback_speed_faster = (
 	$MarginContainer/VBoxContainer/CheckBoxPlaybackSpeedFaster
 )
@@ -44,3 +51,7 @@ func _on_check_box_playback_speed_faster_toggled(toggled_on):
 
 func _on_button_mouse_entered():
 	sfx_mouse_hover.play()
+
+
+func _on_check_box_hide_button_labels_toggled(toggled_on):
+	hide_button_labels_toggle_requested.emit(toggled_on)
