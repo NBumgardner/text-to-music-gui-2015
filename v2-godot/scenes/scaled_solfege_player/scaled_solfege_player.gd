676548684c5aaf5cadd09aa6b100209863c807a3
Noah Bumgardner <noah@playgamecafe.com>
2026-09-15 14:10:49 -0500

    Revert "Fail to Keep Focus on Play Paused and Play Audio"
    This reverts commit 9c35fa9b0b92515b29375507e287014e39d71049.

--- v2-godot/scenes/scaled_solfege_player/scaled_solfege_player.gd
+++ v2-godot/scenes/scaled_solfege_player/scaled_solfege_player.gd
@@ -71,7 +71,6 @@ var _note_index_modulo = 8
 
 
 var _paused = false
-var _playing = false
 
 
 var _queue_of_note_indexes_to_play: Array = []
@@ -87,7 +86,7 @@ func _ready():
 
 
 func _process(delta: float) -> void:
-	if not _playing or _paused:
+	if _paused:
 		return
 
 	_time_since_last_note_started += delta
@@ -110,7 +109,6 @@ func _process(delta: float) -> void:
 ## Stop playing all audio notes of the scene.
 func stop_notes():
 	_queue_of_note_indexes_to_play = []
-	_playing = false
 	_time_since_last_note_started = note_length_seconds
 	audio_stream_player.stop()
 
@@ -164,19 +162,16 @@ func _set_notes_to_play() -> void:
 func _on_button_play_pressed() -> void:
 	if _paused and _is_audible_note(_current_note_index_playing):
 		_paused = false
-		_playing = true
 		_sync_button_visibility_with_paused()
 		_set_audible_volume_and_play_note(_current_note_index_playing)
 		return
 
 	if _paused and _queue_of_note_indexes_to_play.any(_is_audible_note):
 		_paused = false
-		_playing = true
 		_sync_button_visibility_with_paused()
 		return
 
 	_paused = false
-	_playing = true
 	_sync_button_visibility_with_paused()
 
 	play_button_pressed.emit()
@@ -310,7 +305,8 @@ func _set_other_entry_fields(numbers_list_list) -> void:
 
 func _sync_button_visibility_with_paused() -> void:
 	if _paused:
-		_visually_replace_button_pause_with_play()
+		button_pause.hide()
+		button_play.show()
 		return
 
 	if (not _paused
@@ -320,7 +316,8 @@ func _sync_button_visibility_with_paused() -> void:
 		button_play.hide()
 		return
 
-	_visually_replace_button_pause_with_play()
+	button_pause.hide()
+	button_play.show()
 
 
 func _translate_words_list_into_numbers_list_list(word_list):
@@ -346,26 +343,6 @@ func _translate_words_list_into_numbers_list_list(word_list):
 	)
 
 	return numbers_translated_from_word_list
-
-
-func _visually_replace_button_pause_with_play():
-	var button_to_visually_replace_is_focused = button_pause.has_focus()
-
-	button_pause.hide()
-	button_play.show()
-
-	if button_to_visually_replace_is_focused:
-		button_play.grab_focus()
-
-
-func _visually_replace_button_play_with_pause():
-	var button_to_visually_replace_is_focused = button_play.has_focus()
-
-	button_pause.show()
-	button_play.hide()
-
-	if button_to_visually_replace_is_focused:
-		button_pause.grab_focus()
 
 
 func _on_button_mouse_entered():
