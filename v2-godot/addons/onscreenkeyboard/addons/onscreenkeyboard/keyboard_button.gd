4489d1c066eee4d2b99153231ef36910aed4255f
Noah Bumgardner <noah@playgamecafe.com>
2026-09-22 20:50:16 -0500

    Update Addon with Unfinished PR
    Copied from commit 45fb098bc202b8538626e4880c3c1c12f2ac5df1.

--- v2-godot/addons/onscreenkeyboard/addons/onscreenkeyboard/keyboard_button.gd
+++ v2-godot/addons/onscreenkeyboard/addons/onscreenkeyboard/keyboard_button.gd
@@ -1,67 +1,92 @@
 extends Button
 
-var key_data
+var keyData
 
 signal released
 signal down
 
-var icon_tex_rect
+var iconTexRect
+
+var focused = false setget set_focused
+var pressing = false setget set_pressing
+var id_x = 0
+var id_y = 0
+
+func set_focused(_focused):
+	focused = _focused
+	update()
+
+func set_pressing(_pressing):
+	if pressing != _pressing:
+		if _pressing:
+			emit_signal("button_down")
+		else:
+			emit_signal("button_up")
+	pressing = _pressing
+	update()
 
 func _enter_tree():
 	pass
 
-func _ready():
-	pass # Replace with function body.
 
-func _init(_key_data):
-	key_data = _key_data
-	button_down.connect(_on_button_down)
-	button_up.connect(_on_button_up)
-	item_rect_changed.connect(_on_item_rect_changed)
+func _draw():
+	var style = get_stylebox("normal")
+	if pressing or get_draw_mode() == DRAW_PRESSED or (toggle_mode and pressed):
+		draw_style_box(get_stylebox("pressed"), Rect2(Vector2.ZERO, rect_size))
+	else:
+		draw_style_box(style, Rect2(Vector2.ZERO, rect_size))
+	if focused:
+		draw_style_box(get_stylebox("focus"), Rect2(Vector2.ZERO, rect_size))
+	var font = get_font("font")
+	var text_ofs = ((rect_size - style.get_minimum_size() - font.get_string_size(text)) / 2.0) + style.get_offset();
+	text_ofs.y += font.get_ascent();
+	font.draw(get_canvas_item(), text_ofs, text)
 
+func item_rect_changed():
+	if iconTexRect != null:
+		iconTexRect.rect_size = rect_size
+
+func _init(_keyData):
+	keyData = _keyData
+	connect("button_up",self,"button_up")
+	connect("button_down",self,"button_down")
+	connect("item_rect_changed",self,"item_rect_changed")
+	
 	size_flags_horizontal = SIZE_EXPAND_FILL
 	size_flags_vertical = SIZE_EXPAND_FILL
-
+	
 	focus_mode = FOCUS_NONE
+	
+	if keyData.has("display"):
+		text = keyData.get("display")
 
-	if key_data.has("display"):
-		text = key_data.get("display")
-
-	if key_data.has("stretch-ratio"):
-		size_flags_stretch_ratio = key_data.get("stretch-ratio")
-
-
-func set_icon_color(color):
-	if icon_tex_rect != null:
-		icon_tex_rect.modulate = color
+	if keyData.has("stretch-ratio"):
+		size_flags_stretch_ratio = keyData.get("stretch-ratio")
 
 
-func set_icon(texture):
-	icon_tex_rect = TextureRect.new()
-	icon_tex_rect.ignore_texture_size = true
-	icon_tex_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-	icon_tex_rect.texture = texture
-	add_child(icon_tex_rect)
+func setIconColor(color):
+	if iconTexRect != null:
+		iconTexRect.modulate = color
+
+func setIcon(texture):
+	iconTexRect = TextureRect.new()
+	iconTexRect.expand = true
+	iconTexRect.stretch_mode = 6
+	iconTexRect.texture = texture
+	add_child(iconTexRect)
 
 
-func change_uppercase(value):
+func changeUppercase(value):
 	if value:
-		if key_data.has("display-uppercase"):
-			text = key_data.get("display-uppercase")
+		if keyData.has("display-uppercase"):
+			text = keyData.get("display-uppercase")
 	else:
-		if key_data.has("display"):
-			text = key_data.get("display")
+		if keyData.has("display"):
+			text = keyData.get("display")
 
 
-func _on_item_rect_changed():
-	if icon_tex_rect != null:
-		icon_tex_rect.size = size
-
-
-func _on_button_up():
-	released.emit(key_data)
-	release_focus()
-
-
-func _on_button_down():
-	down.emit(key_data)
+func button_up():
+	emit_signal("released",keyData,id_x,id_y)
+	
+func button_down():
+	emit_signal("down",keyData,id_x,id_y)
